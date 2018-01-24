a4d5f498e1fd254c42c2fe27d30fe7aff1ad4b63
Rodrigo Barbaresco <barbaresco.rodrigo@gmail.com>
2018-01-24 20:29:54 -0200

    Accessing some information from server.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import requests
 
 app = Flask(__name__, static_folder="static/dist", template_folder="static")
 
@@ -8,10 +9,15 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/apis")
+def apis():
+    return jsonify(requests.get("http://localhost:5000/apispec_1.json").json())
+
+
 @app.route("/hello")
 def hello():
     return "Hello World!!!!!!!!!!!!!!!!!!!!!!!"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
--- static/js/App.jsx
+++ static/js/App.jsx
@@ -1,7 +1,46 @@
 // App.jsx
 import React from "react";
+
+var $ = require('jquery');
+
 export default class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
+  constructor() {
+        super();
+
+        this.state = {
+          apis: null,
+        };
+
+        $.get('/apis', (data) => {
+            this.setState({
+                apis: data
+            });
+        });
   }
+
+  selectedCity(event, {suggestion}) {
+      this.setState({
+          forecast: null
+      });
+      $.get('/forecast/' + suggestion['id'], (data) => {
+          this.setState({
+              forecast: JSON.parse(data)
+          });
+      });
+  }
+
+  render () {
+      var apis = null;
+      console.log('look!', this.state);
+      if (this.state.apis) {
+          apis = <p>{this.state.apis}</p>
+      }
+
+      return (
+          <div className='header-contents'>
+              <div className='content-title'>apismo</div>
+              {apis}
+          </div>
+      );
+  }	
 }
